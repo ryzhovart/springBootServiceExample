6bf8968298d1c357cac1b365d43f34fddf47ca9c
ryzhovart <artyomryzhov@gmail.com>
2023-10-09 18:02:39 +0200

    Add internationalization i18n

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Swagger: /swagger-ui/index.htm
 api docs: /v3/api-docs
 
 for xml response add header Accept:application/xml
+for getting message in fr locale add header Accept-Language:fr
--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "hello world";
@@ -21,4 +30,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    }
+
 }
